1d65ad349f9656be7d4abb5d955eeb7be54ac020
Grace Willhoite <gewillho@artannex103-14.ucdavis.edu>
2020-02-01 16:16:39 -0800

    Gardener moves and collides with red box

---
+++ Assets/Tile_State.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile_State : MonoBehaviour
+{
+
+    private int state = 0;
+    public Sprite seeded;
+    public Sprite grown;
+    public SpriteRenderer spr;
+
+
+    public void seed() {
+        if (state == 0)
+        {
+            this.state = 1;
+            spr.sprite = seeded;
+        }
+    }
+
+    public void grow() {
+        if (state == 1)
+        {
+            this.state = 2;
+            spr.sprite = grown;
+        }
+    }
+}
